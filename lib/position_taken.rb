15b90de1d13be963a2c84ea1683ff2b2771c11db
Paula Tulis <tulis.paula@gmail.com>
2019-02-17 18:14:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 
 def position_taken?(board, index)
   position = board[index]
-  if position == ""|| position = " "|| position = nil
+  if position == ""|| position == " "|| position == nil
     return false
   else
     return true
